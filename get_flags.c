731525f4cc36de14637bd0c2afeaa4926afc2b1a
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-11-22 16:25:30 +0100

    function printf4

--- get_flags.c
+++ get_flags.c
@@ -10,18 +10,17 @@ int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int j, this_i;
+	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {FLAGS_MINUS, FLAGS_PLUS, 
-		FLAGS_ZERO, FLAGS_HASH, FLAGS_SPACE, 0};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (this_i = *i + 1; format[this_i] != '\0'; this_i++)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[this_i] == FLAGS_CH[j])
+			if (format[curr_i] == FLAGS_CH[j])
 			{
-				flags = flags|FLAGS_ARR[j];
+				flags |= FLAGS_ARR[j];
 				break;
 			}
 
@@ -29,7 +28,7 @@ int get_flags(const char *format, int *i)
 			break;
 	}
 
-	*i = this_i - 1;
+	*i = curr_i - 1;
 
 	return (flags);
 }
